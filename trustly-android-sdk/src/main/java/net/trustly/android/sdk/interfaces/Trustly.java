cc853d5609f3e1f116c4a055275400f806beec34
Andre Guedes <andre.guedes@trustly.com>
2023-05-12 16:02:04 -0300

    Removing confidential headers

--- trustly-android-sdk/src/main/java/net/trustly/android/sdk/interfaces/Trustly.java
+++ trustly-android-sdk/src/main/java/net/trustly/android/sdk/interfaces/Trustly.java
@@ -1,19 +1,3 @@
-/*  ___________________________________________________________________________________________________________
- *
- *    TRUSTLY CONFIDENTIAL AND PROPRIETARY INFORMATION
- *  ___________________________________________________________________________________________________________
- *
- *      Copyright (c) 2012 - 2020 Trustly
- *      All Rights Reserved.
- *
- *   NOTICE:  All information contained herein is, and remains, the confidential and proprietary property of
- *   Trustly and its suppliers, if any. The intellectual and technical concepts contained herein are the
- *   confidential and proprietary property of Trustly and its suppliers and  may be covered by U.S. and
- *   Foreign Patents, patents in process, and are protected by trade secret or copyright law. Dissemination of
- *   this information or reproduction of this material is strictly forbidden unless prior written permission is
- *   obtained from Trustly.
- *   ___________________________________________________________________________________________________________
- */
 package net.trustly.android.sdk.interfaces;
 
 import android.content.Context;
