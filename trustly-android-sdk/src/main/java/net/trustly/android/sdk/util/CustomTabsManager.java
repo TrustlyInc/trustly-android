d2778d5cdedfa474536b91d67f5e5c1f6187d783
Andre Guedes <andre.guedes@trustly.com>
2024-02-20 11:57:40 -0300

    Adding exception handler

--- trustly-android-sdk/src/main/java/net/trustly/android/sdk/util/CustomTabsManager.java
+++ trustly-android-sdk/src/main/java/net/trustly/android/sdk/util/CustomTabsManager.java
@@ -3,6 +3,7 @@ package net.trustly.android.sdk.util;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.browser.customtabs.CustomTabsIntent;
 
@@ -15,11 +16,15 @@ public class CustomTabsManager {
     }
 
     public static void openCustomTabsIntent(Context context, String url) {
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.intent.setPackage(context.getString(R.string.chrome_package));
-        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        customTabsIntent.launchUrl(context, Uri.parse(url));
+        try {
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.intent.setPackage(context.getString(R.string.chrome_package));
+            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            customTabsIntent.launchUrl(context, Uri.parse(url));
+        } catch (Exception e) {
+            Log.e("CustomTabsManager", e.toString());
+        }
     }
 
 }
