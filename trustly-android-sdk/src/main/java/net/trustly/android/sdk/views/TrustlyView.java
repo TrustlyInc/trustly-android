91f38fcb80e5dd4e910159e81cfadfcdd36be88a
Andre Guedes <andre.guedes@trustly.com>
2024-07-15 14:30:40 -0300

    Adding new methods that uses strings as parameters

--- trustly-android-sdk/src/main/java/net/trustly/android/sdk/views/TrustlyView.java
+++ trustly-android-sdk/src/main/java/net/trustly/android/sdk/views/TrustlyView.java
@@ -357,6 +357,11 @@ public class TrustlyView extends LinearLayout implements Trustly {
         return this;
     }
 
+    @Override
+    public Trustly establish(String establishData) {
+        return establish(new HashMap<>());
+    }
+
     public String getInAppBrowserLaunchURL(Map<String, String> establishData) {
         Map<String, String> data = new HashMap<>(establishData);
         String deviceType = establishData.get("deviceType");
@@ -441,6 +446,11 @@ public class TrustlyView extends LinearLayout implements Trustly {
         } catch (Exception e) {
         }
         return this;
+    }
+
+    @Override
+    public Trustly selectBankWidget(String establishData) {
+        return selectBankWidget(new HashMap<>());
     }
 
     @Override
