78b5ec5fdf73f2c7205e1e775474f70784722e90
Andre Guedes <andre.guedes@trustly.com>
2024-08-01 16:25:44 -0300

    Adding data model

---
+++ trustly-android-sdk/src/main/java/net/trustly/android/sdk/data/Setting.java
@@ -0,0 +1,13 @@
+package net.trustly.android.sdk.data;
+
+public class Setting {
+
+    public LightBoxSetting lightBox;
+
+    class LightBoxSetting {
+
+        public String context;
+
+    }
+
+}
