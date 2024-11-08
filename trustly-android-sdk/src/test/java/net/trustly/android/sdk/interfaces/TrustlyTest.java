7f4aac1b6f94a55cc42ac257667f23cc8269ac79
Andre Guedes <andre.guedes@trustly.com>
2024-11-08 18:32:31 -0300

    Updating trustly interface tests

--- trustly-android-sdk/src/test/java/net/trustly/android/sdk/interfaces/TrustlyTest.java
+++ trustly-android-sdk/src/test/java/net/trustly/android/sdk/interfaces/TrustlyTest.java
@@ -2,6 +2,7 @@ package net.trustly.android.sdk.interfaces;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.clearInvocations;
@@ -51,6 +52,11 @@ public class TrustlyTest {
     @After
     public void tearDown() {
         clearInvocations(mockTrustly, mockTrustlyCallback, mockTrustlyListener, mockContext);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTrustlyInstanceIsCalled() {
+        assertThrows(NullPointerException.class, () -> Trustly.Instance.create(mockContext));
     }
 
     @Test
