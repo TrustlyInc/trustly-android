5676785e0d8b04a24a682ecd86590849952bfdd8
Andre Guedes <andre.guedes@trustly.com>
2025-08-11 10:25:16 -0300

    Verifying package name to log error

--- trustly-android-sdk/src/main/java/net/trustly/android/sdk/util/error/TrustlyExceptionHandler.kt
+++ trustly-android-sdk/src/main/java/net/trustly/android/sdk/util/error/TrustlyExceptionHandler.kt
@@ -10,30 +10,32 @@ import java.util.Calendar
 class TrustlyExceptionHandler : Thread.UncaughtExceptionHandler {
 
     override fun uncaughtException(thread: Thread, exception: Throwable) {
-        val errorMessage = StringBuilder()
-        errorMessage.apply {
-            append(NEW_LINE)
-            append("Trustly SDK Version: ")
-            append(BuildConfig.SDK_VERSION)
-            append(NEW_LINE)
-            append("Android SDK Version: ")
-            append(Build.VERSION.SDK_INT)
-            append(NEW_LINE)
-            append("Android Version Release: ")
-            append(Build.VERSION.RELEASE)
-            append(NEW_LINE)
-            append("Device Model: ")
-            append(Build.MODEL)
-            append(NEW_LINE)
-            append("Date: ")
-            append(Calendar.getInstance().time)
-            append(NEW_LINE)
-            val stackTrace = StringWriter()
-            exception.printStackTrace(PrintWriter(stackTrace))
-            append(stackTrace.toString())
-        }
+        if (exception.stackTraceToString().contains(BuildConfig.LIBRARY_PACKAGE_NAME)) {
+            val errorMessage = StringBuilder()
+            errorMessage.apply {
+                append(NEW_LINE)
+                append("Trustly SDK Version: ")
+                append(BuildConfig.SDK_VERSION)
+                append(NEW_LINE)
+                append("Android SDK Version: ")
+                append(Build.VERSION.SDK_INT)
+                append(NEW_LINE)
+                append("Android Version Release: ")
+                append(Build.VERSION.RELEASE)
+                append(NEW_LINE)
+                append("Device Model: ")
+                append(Build.MODEL)
+                append(NEW_LINE)
+                append("Date: ")
+                append(Calendar.getInstance().time)
+                append(NEW_LINE)
+                val stackTrace = StringWriter()
+                exception.printStackTrace(PrintWriter(stackTrace))
+                append(stackTrace.toString())
+            }
 
-        Log.e(TAG, errorMessage.toString())
+            Log.e(TAG, errorMessage.toString())
+        }
     }
 
     companion object {
